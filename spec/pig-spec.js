7d139a3c5cc4c236604bb2a71146b22486f86537
Neha Malhotra and Susan Kralian <susan@>
2019-09-04 16:18:55 -0700

    mainly empty files with unit testing on arrays, sentences, math

--- spec/pig-spec.js
+++ spec/pig-spec.js
@@ -1,6 +1,35 @@
-import {
+import { add } from './../src/pig.js'
+import { sentence } from './../src/pig.js'
+import { cuteArray } from './../src/pig.js'
+// import {
+//
+// } from './../src/pig.js'
 
-} from './../src/pig.js'
-import {
-  Player
-} from './../src/pig.js'
+describe ('add', function() {
+  it ('expect pass/should correctly determine if the sum is 9', function (){
+    var a = 4;
+    var b = 5;
+    expect(add(a,b)).toEqual(9);
+  })
+  it ('expect pass/should correctly determine if the sum is not equal to 8', function (){
+    var a = 4;
+    var b = 5;
+
+    expect(add(a,b)).not.toEqual(8);
+    })
+  });
+describe ('sentence', function() {
+    it ('expect pass/should be true that the sentence this is a sentence includes is', function (){
+      var c = "this is a sentence";
+
+      expect(sentence(c)).toEqual(true);
+      })
+});
+
+describe ('cuteArray', function() {
+    it ('expect pass/should have 5 at the front of the new Array', function (){
+      var d = [0,5,6,9];
+
+      expect(cuteArray(d)).toEqual(5);
+      })
+});
---
+++ src/pig.js
@@ -0,0 +1,22 @@
+
+export function add (a,b){
+  return a + b
+}
+
+export function sentence (c){
+  return c.includes("is");
+}
+
+export function cuteArray (d){
+  d.unshift(5)
+  return d[0]
+}
+
+// function divide ){
+//   return a/b
+// }
+//
+//
+// function calculator (operator) {
+//   return add
+// }
